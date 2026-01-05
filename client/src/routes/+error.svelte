31f2dafa171a105ee31cafd0d8bba536afd634c7
pls <124419933+plsuwu@users.noreply.github.com>
2026-01-06 08:02:32 +1000

    Update +error.svelte

--- client/src/routes/+error.svelte
+++ client/src/routes/+error.svelte
@@ -13,7 +13,7 @@
 	<div class="mb-12 text-lg font-medium">there was an error: {err}</div>
 	<div>i'm genuinely not even sorry</div>
 	<div>this is a prototype frontend get OFF my case</div>
-	<a class="text-blue-500 transition-opacity duration-150 hover:opacity-40" href="/"
+	<a class="text-blue-500 transition-opacity duration-150 hover:opacity-40" href="https://piss.fan"
 		>{'<--'} go home.</a
 	>
 </div>
