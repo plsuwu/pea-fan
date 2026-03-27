a04fbcef1974f607e07b55d53657a90377c3d8ef
plsuwu <124419933+plsuwu@users.noreply.github.com>
2026-03-27 21:23:07 +1000

    add preview announcement thing

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -35,15 +35,15 @@
 		return `https://${channel}.${ROOT_HOSTNAME}`;
 	}
 
-    function lPad(n: number) {
-        let text = (n).toString(); 
+	function lPad(n: number) {
+		let text = n.toString();
 
-        if (text.length < 2) {
-            return '&nbsp;' + text
-        } 
+		if (text.length < 2) {
+			return '&nbsp;' + text;
+		}
 
-        return text;
-    }
+		return text;
+	}
 </script>
 
 {#if data.count === 1 && total && channelData}
@@ -51,32 +51,51 @@
 		<ChannelRoute {channel} {total} {color} {leaderboard} />
 	</div>
 {:else}
-	<div class="h-full w-full pt-8 sm:w-3/4 xl:w-2/4 2xl:w-2/6 overflow-x-hidden">
+	<div class="h-full w-full overflow-x-hidden pt-8 sm:w-3/4 xl:w-2/4 2xl:w-2/6">
 		<div class="items-center justify-center py-8 text-center text-4xl font-black">
 			PISS LEADERBOARD
 		</div>
-        <div class="border rounded-md p-1 w-full">
-		{#each sortByLiveState(channels) as channel, i}
-			<a
-				href={`${getChannelUri(channel.channel)}`}
-				style={`background-color: ${i % 2 !== 0 ? '#4a556550' : ''};`}
-				class="flex flex-row px-1 py-0.5 transition-opacity duration-150 hover:opacity-50 items-center justify-center"
-			>
-				{#if i === 0}
-					<Crown size={22} class="mx-1 lg:mx-4" />
-				{:else}
-					<Crown size={22} class="invisible mx-1 lg:mx-4" />
-				{/if}
-				<div class="pr-1 text-end font-mono">{@html lPad(i + 1)}.</div>
-				<div class="flex w-full flex-row justify-between">
-					<div class="pl-1 flex w-full max-w-[175px] flex-row items-center justify-between">
-						<div class="font-display text-lg">{channel.channel}</div>
-						<Live isLive={channel.live} name={channel.channel} />
+		<div class="mb-4 rounded-md border border-amber-600 px-3 py-1 text-center text-amber-700">
+			hello there is a new leaderboard frontend that is very close to finished (like under a day
+			away?? two maybe? idk im not good at "The Time" or finishing things) anyway i put up a preview
+			version on
+			<a class="text-blue-600 hover:opacity-50" href="https://rat.moe" target="_blank">rat.moe</a>
+			please look if you want ok thank you thanks th
+		</div>
+		<!-- <div class="px-8 pb-8 font-bold"> -->
+		<!-- 	exciting new features such as: -->
+		<!-- 	<ul class="list-inside list-disc font-normal"> -->
+		<!-- 		<li> -->
+		<!-- 			NO LONGER CRASHES 4 to 5 times at 6am on a tuesday when half the leaderboard is streaming! -->
+		<!-- 		</li> -->
+		<!-- 		<li>NO LONGER LOSES your pisscount if you change your name on twitch!</li> -->
+		<!-- 	</ul> -->
+		<!-- </div> -->
+		<div class="w-full rounded-md border p-1">
+			{#each sortByLiveState(channels) as channel, i}
+				<a
+					href={`${getChannelUri(channel.channel)}`}
+					style={`background-color: ${i % 2 !== 0 ? '#4a556550' : ''};`}
+					class="flex flex-row items-center justify-center px-1 py-0.5 transition-opacity duration-150 hover:opacity-50"
+				>
+					{#if i === 0}
+						<Crown size={22} class="mx-1 lg:mx-4" />
+					{:else}
+						<Crown size={22} class="invisible mx-1 lg:mx-4" />
+					{/if}
+					<div class="pr-1 text-end font-mono">{@html lPad(i + 1)}.</div>
+					<div class="flex w-full flex-row justify-between">
+						<div class="flex w-full max-w-[175px] flex-row items-center justify-between pl-1">
+							<div class="font-display text-lg">{channel.channel}</div>
+							<Live isLive={channel.live} name={channel.channel} />
+						</div>
+						<div class="pr-0 lg:pr-4">
+							({channel.total}
+							{channel.total === '1' ? 'time' : 'times'})
+						</div>
 					</div>
-					<div class="pr-0 lg:pr-4">({channel.total} {channel.total === '1' ? 'time' : 'times'})</div>
-				</div>
-			</a>
-		{/each}
-        </div>
+				</a>
+			{/each}
+		</div>
 	</div>
 {/if}
