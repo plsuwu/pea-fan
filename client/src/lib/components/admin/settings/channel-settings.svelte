1aec1c6353fc906e4541d9b206456cf7dc4cf3df
plsuwu <124419933+plsuwu@users.noreply.github.com>
2026-05-03 23:04:49 +1000

    disable single-channel hook reset button, add channel state indicators

--- client/src/lib/components/admin/settings/channel-settings.svelte
+++ client/src/lib/components/admin/settings/channel-settings.svelte
@@ -4,7 +4,6 @@
 	import { CloudSyncIcon, FishingHookIcon } from "@lucide/svelte";
 	// import { Snippet } from "svelte";
 	import ActionHandler from "./action-handler.svelte";
-	import Hooks from "./hooks.svelte";
 
 	let { config, waiting, setWaiting } = $props();
 </script>
@@ -19,15 +18,7 @@
 <!-- {/snippet} -->
 
 <div class="flex w-[220px] flex-col justify-items-center space-y-2.5">
-	<ActionHandler
-		{waiting}
-		{setWaiting}
-		value={config.id}
-		enabled={!waiting}
-		name="channel-id"
-		action="?/bot"
-		label="replying"
-	>
+	<ActionHandler {setWaiting} action="?/bot" label="replying">
 		<input type="hidden" name="channel-id" value={config.id} />
 		<Checkbox
 			id={config.id}
@@ -40,15 +31,7 @@
 		/>
 	</ActionHandler>
 
-	<ActionHandler
-		{waiting}
-		{setWaiting}
-		value={config.id}
-		enabled={!waiting}
-		name="channel-id"
-		action="?/sync"
-		label="resync state"
-	>
+	<ActionHandler {setWaiting} action="?/sync" label="resync state">
 		<div class="flex flex-row items-center">
 			<button
 				value={config.id}
@@ -62,21 +45,14 @@
 		</div>
 	</ActionHandler>
 
-	<ActionHandler
-		{waiting}
-		{setWaiting}
-		value={config.id}
-		enabled={!waiting}
-		name="channel-id"
-		action="?/sync"
-		label="reset hook"
-	>
+	<ActionHandler {setWaiting} action="?/fake_action" label="reset hook">
 		<button
 			value={config.id}
-			class="items-center transition-all duration-150 hover:brightness-50"
+			class="items-center transition-all duration-150 hover:brightness-50
+            disabled:brightness-50 disabled:hover:brightness-50"
 			name="channel-id"
 			type="submit"
-			disabled={waiting}
+			disabled={true}
 		>
 			<FishingHookIcon size={20} />
 		</button>
